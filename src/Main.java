6accf7c3b71ab42edb74185f5f4806845c1c08a8
ismael <fmartin@nigul.cide.es>
2021-11-19 21:40:18 +0100

    Seguimos intentando el formateo temporal de xml

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import org.xml.sax.SAXException;
 
+import javax.swing.*;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.xpath.XPathExpressionException;
 import java.io.IOException;
@@ -10,12 +12,25 @@ public class Main {
 
         try {
             String nameFile = "Prueba";
-            XmlManagerDomXpath manager = new XmlManagerDomXpath();
-//            manager.createXML("Prueba");
-//            manager.inputData("1","Juan","Eso","25/08/1999", "Cide");
-//            manager.inputData("2","Juan2","Eso","25/08/1999", "Cide");
-//            manager.inputData("3","Juan3","Eso","25/08/1999", "Cide");
-            manager.showAllXml(nameFile);
+            XmlManager manager = new XmlManager();
+            String op = JOptionPane.showInputDialog("Op?");
+            switch (op) {
+                case "1":
+                    manager.createXML("Prueba");
+                    break;
+                case "2":
+                    manager.inputData(nameFile,JOptionPane.showInputDialog("Code?"), "Juan", "Eso", "25/08/1999", "Cide");
+                    break;
+                case "3":
+                    manager.showAllXml(nameFile);
+                    break;
+                case "4":
+                    manager.modifyData(nameFile, JOptionPane.showInputDialog("Code?"));
+                    break;
+
+            }
+
+
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -23,6 +38,10 @@ public class Main {
         } catch (SAXException e) {
             e.printStackTrace();
         } catch (XPathExpressionException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
             e.printStackTrace();
         }
     }
